503977c9f6defafb7d2e28afab93af7a4139d9ad
Fyfeu Feuzme <feuzme@gmail.com>
2021-04-29 14:38:01 +0200

    inscription feature in progress

---
+++ scripts/inscription.js
@@ -0,0 +1,4 @@
+var inscription = (form) =>{
+    alert("Vous vous appelez " + form.nom.value + " " + form.prenom.value + " vous avez " + form.age.value + " ans, êtes un(e) "
+    + form.sex.value + " vous habitez " + form.city.value);
+}
